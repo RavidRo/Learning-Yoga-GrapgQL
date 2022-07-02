1ffef9ca94b0e9d37f1c7881b8b71fd456b734ad
Ravid Rom <romravid@gmail.com>
2022-07-02 20:24:04 +0300

    A Simple Query

--- src/schema.ts
+++ src/schema.ts
@@ -2,13 +2,27 @@ import { makeExecutableSchema } from '@graphql-tools/schema';
 
 const typeDefinitions = /* GraphQL */ `
 	type Query {
-		hello: String!
+		info: String!
+		feed: [Link!]!
+	}
+
+	type Link {
+		id: ID!
+		description: String!
+		url: String!
 	}
 `;
 
 const resolvers = {
 	Query: {
-		hello: () => 'Hello World!',
+		info: () => 'This is the API of Hacker News Clone',
+		feed: () => links,
+	},
+
+	Link: {
+		id: (link: Link) => link.id,
+		description: (link: Link) => link.description,
+		url: (link: Link) => link.url,
 	},
 };
 
@@ -16,3 +30,18 @@ export const schema = makeExecutableSchema({
 	resolvers: [resolvers],
 	typeDefs: [typeDefinitions],
 });
+
+// ********* Dummy Data *********
+type Link = {
+	id: string;
+	description: string;
+	url: string;
+};
+
+const links: Link[] = [
+	{
+		id: 'link-0',
+		url: 'https://graphql-yoga.com',
+		description: 'The easiest way of setting up a GraphQL server',
+	},
+];
