77f660f82d18d08b8039a8b2d5f94acf9061d680
Ravid Rom <romravid@gmail.com>
2022-07-02 22:33:24 +0300

    Graph Relations

--- src/schema.ts
+++ src/schema.ts
@@ -1,28 +1,53 @@
 import { makeExecutableSchema } from '@graphql-tools/schema';
+import { Comment, Link } from '@prisma/client';
 import { GraphQLContext } from './context';
 
 const typeDefinitions = /* GraphQL */ `
 	type Query {
 		info: String!
 		feed: [Link!]!
+		comment(id: ID!): Comment
+		link(id: ID!): Link
 	}
 
 	type Mutation {
 		postLink(url: String!, description: String!): Link!
+		postCommentOnLink(linkId: ID!, body: String!): Comment!
 	}
 
 	type Link {
 		id: ID!
 		description: String!
 		url: String!
+		comments: [Comment!]!
+	}
+
+	type Comment {
+		id: ID!
+		body: String!
+		link: Link
 	}
 `;
 
 const resolvers = {
+	Link: {
+		comments: (parent: Link, _args: {}, context: GraphQLContext) =>
+			context.prisma.comment.findMany({ where: { link: { id: parent.id } } }),
+	},
+
+	Comment: {
+		link: (parent: Comment, _args: {}, context: GraphQLContext) =>
+			parent.linkId ? context.prisma.link.findFirst({ where: { id: parent.linkId } }) : null,
+	},
+
 	Query: {
 		info: () => 'This is the API of Hacker News Clone',
 		feed: (_parent: unknown, _args: {}, context: GraphQLContext) =>
 			context.prisma.link.findMany(),
+		comment: (_parent: unknown, args: { id: string }, context: GraphQLContext) =>
+			context.prisma.comment.findFirst({ where: { id: parseInt(args.id) } }),
+		link: (_parent: unknown, args: { id: string }, context: GraphQLContext) =>
+			context.prisma.link.findFirst({ where: { id: parseInt(args.id) } }),
 	},
 
 	Mutation: {
@@ -39,6 +64,20 @@ const resolvers = {
 			});
 
 			return link;
+		},
+		postCommentOnLink: async (
+			_parent: unknown,
+			args: { linkId: string; body: string },
+			context: GraphQLContext
+		) => {
+			const newComment = await context.prisma.comment.create({
+				data: {
+					body: args.body,
+					linkId: parseInt(args.linkId),
+				},
+			});
+
+			return newComment;
 		},
 	},
 };
